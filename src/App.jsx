e549956f2c046b21630a356ef5090ac4162eada3
ahmed osman <ahmedmido19782@gmail.com>
2023-12-30 19:46:18 +0200

    add: Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -61,12 +61,12 @@ function App() {
         </div>
 
         <div className="buttons">
-          <a className='twitter' target="_blank" href={url} style={backgroundColor} >
+          <a className='twitter' id="tweet-quote" target="_blank" href={url} style={backgroundColor} >
             <svg  xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
               <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
             </svg>
           </a>
-          <button className='button' onClick={getQuote} style={{backgroundColor:`#${color}`}}>New Quote</button>
+          <button className='button' id="new-quote" onClick={getQuote} style={{backgroundColor:`#${color}`}}>New Quote</button>
         </div>
         
       </div>
